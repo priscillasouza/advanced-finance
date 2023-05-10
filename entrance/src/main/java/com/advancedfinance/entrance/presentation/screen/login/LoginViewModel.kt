df7808aca67a9a654f64ffec8e918474834c7bf3
Priscilla Souza <priscillasilva-24@hotmail.com>
2023-05-10 15:06:23 -0300

    implementando login com biometria

--- entrance/src/main/java/com/advancedfinance/entrance/presentation/screen/login/LoginViewModel.kt
+++ entrance/src/main/java/com/advancedfinance/entrance/presentation/screen/login/LoginViewModel.kt
@@ -1,7 +1,31 @@
 package com.advancedfinance.entrance.presentation.screen.login
 
+import androidx.biometric.BiometricManager
 import com.advancedfinance.core.platform.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class LoginViewModel: BaseViewModel() {
+class LoginViewModel : BaseViewModel() {
 
+    private val stateFlowBiometricMutable = MutableStateFlow<StateBiometric>(StateBiometric.Loading)
+    val stateFlowBiometric: StateFlow<StateBiometric> = stateFlowBiometricMutable
+
+    fun checkStateBiometric(state: Int) {
+        when (state) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                stateFlowBiometricMutable.value = StateBiometric.BiometricSuccess
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                stateFlowBiometricMutable.value = StateBiometric.BiometricErrorNoHardware
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                stateFlowBiometricMutable.value = StateBiometric.BiometricErrorNoneEnrolled
+            }
+        }
+    }
+
+    sealed class StateBiometric {
+        object Loading : StateBiometric()
+        object BiometricSuccess : StateBiometric()
+        object BiometricErrorNoHardware : StateBiometric()
+        object BiometricErrorNoneEnrolled : StateBiometric()
+    }
 }
